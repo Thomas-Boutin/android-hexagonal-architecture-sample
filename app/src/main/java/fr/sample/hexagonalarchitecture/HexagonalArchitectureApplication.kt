388eb88d59c4613efe31ec5dec1c0d40f7b9d4a4
tboutin <tboutin@ippon.fr>
2021-12-19 13:34:04 +0100

    feat(di): initialize hilt app

---
+++ app/src/main/java/fr/sample/hexagonalarchitecture/HexagonalArchitectureApplication.kt
@@ -0,0 +1,7 @@
+package fr.sample.hexagonalarchitecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HexagonalArchitectureApplication: Application()
