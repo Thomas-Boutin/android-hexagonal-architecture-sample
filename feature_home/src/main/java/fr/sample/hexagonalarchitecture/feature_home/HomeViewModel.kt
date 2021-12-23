b098f535e3d0787d4c91be14cb564f6d59d90807
tboutin <tboutin@ippon.fr>
2021-12-24 00:12:28 +0100

    feat(test): add mockk and first tu

--- feature_home/src/main/java/fr/sample/hexagonalarchitecture/feature_home/HomeViewModel.kt
+++ feature_home/src/main/java/fr/sample/hexagonalarchitecture/feature_home/HomeViewModel.kt
@@ -2,7 +2,9 @@ package fr.sample.hexagonalarchitecture.feature_home
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +15,8 @@ class HomeViewModel @Inject constructor() : ViewModel() {
         "gaga"
     )
         private set
+
+    fun fetchCharacters() = viewModelScope.launch {
+
+    }
 }
---
+++ feature_home/src/test/java/fr/sample/hexagonalarchitecture/feature_home/HomeViewModelTest.kt
@@ -0,0 +1,6 @@
+package fr.sample.hexagonalarchitecture.feature_home
+
+class HomeViewModelTest {
+
+
+}
