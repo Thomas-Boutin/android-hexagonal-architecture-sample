98e641d264a836acd4245c2ffe7901f8172ca89a
tboutin <tboutin@ippon.fr>
2021-12-24 15:21:56 +0100

    feat(test): add coroutines and coroutines test

--- feature_home/src/test/java/fr/sample/hexagonalarchitecture/feature_home/HomeViewModelTest.kt
+++ feature_home/src/test/java/fr/sample/hexagonalarchitecture/feature_home/HomeViewModelTest.kt
@@ -1,6 +1,50 @@
 package fr.sample.hexagonalarchitecture.feature_home
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
 class HomeViewModelTest {
 
+    lateinit var viewModel: HomeViewModel
 
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(StandardTestDispatcher())
+        viewModel = HomeViewModel()
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `should fetch new characters`() = runTest {
+        viewModel.fetchCharacters()
+
+        advanceUntilIdle()
+
+        assertThat(viewModel.characters).containsExactly(
+            "bob",
+            "pamela",
+            "gaga"
+        )
+    }
+
+    @Test
+    fun `should propagate error`() = runTest {
+        viewModel.fetchCharacters()
+
+        advanceUntilIdle()
+
+        assertThat(viewModel.characters).containsExactly(
+            "bob",
+            "pamela",
+            "gaga"
+        )
+    }
 }
