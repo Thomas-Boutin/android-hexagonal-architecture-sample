6ae72bdeae3de33c4cda6e0b528db5a613dd2f7b
tboutin <tboutin@ippon.fr>
2021-12-25 19:22:33 +0100

    feat(api): add apollo client

--- core_characters/src/main/java/fr/sample/hexagonalarchitecture/core_characters/adapter/output/http/GraphQLCharactersOutputAdapter.kt
+++ core_characters/src/main/java/fr/sample/hexagonalarchitecture/core_characters/adapter/output/http/GraphQLCharactersOutputAdapter.kt
@@ -1,11 +1,17 @@
 package fr.sample.hexagonalarchitecture.core_characters.adapter.output.http
 
+import fr.sample.hexagonalarchitecture.commons_io.OutputAdapter
+import fr.sample.hexagonalarchitecture.commons_io.OutputAdapterScopeMain
+import fr.sample.hexagonalarchitecture.commons_io.OutputAdapterScopeWorker
 import fr.sample.hexagonalarchitecture.core_characters.application.port.output.GetCharactersPort
-import fr.sample.hexagonalarchitecture.core_characters.domain.Character
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class GraphQLCharactersOutputAdapter @Inject constructor(): GetCharactersPort {
-    override suspend fun getCharacters(): List<Character> {
-        TODO("Not yet implemented")
+class GraphQLCharactersOutputAdapter @Inject constructor(
+    override val adapterScopeMain: OutputAdapterScopeMain,
+    override val adapterScopeWorker: OutputAdapterScopeWorker
+): OutputAdapter, GetCharactersPort {
+    override suspend fun getCharacters() = withContext(adapterScopeWorker.coroutineContext) {
+        TODO() //GetCharactersQuery().
     }
 }
