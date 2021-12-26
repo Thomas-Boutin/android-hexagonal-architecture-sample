e0e1ddfffa92aee4cf6e8a2bfd47a3ec82dfca56
tboutin <tboutin@ippon.fr>
2021-12-26 18:32:02 +0100

    feat(character-detail): configure rest clients

--- app/src/main/java/fr/sample/hexagonalarchitecture/config/HttpModule.kt
+++ app/src/main/java/fr/sample/hexagonalarchitecture/config/HttpModule.kt
@@ -1,22 +1,39 @@
 package fr.sample.hexagonalarchitecture.config
 
 import com.apollographql.apollo3.ApolloClient
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import fr.sample.hexagonalarchitecture.BuildConfig
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object HttpModule {
+    private const val CONTENT_TYPE_JSON = "application/json"
+
     @Provides
     @Singleton
     fun provideApolloClient(): ApolloClient {
         return ApolloClient
             .Builder()
-            .serverUrl(BuildConfig.GRAPHQL_API_URL)
+            .serverUrl(BuildConfig.API_URL_GRAPHQL)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit  {
+        val contentType = CONTENT_TYPE_JSON.toMediaType()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL_REST)
+            .addConverterFactory(Json.asConverterFactory(contentType))
             .build()
     }
 }
